7865af612c0a0b5e3a2878fc4f3b51eecd997676
nsqws <118088453+IvanParada@users.noreply.github.com>
2026-01-16 22:44:11 -0300

    add custom guard and decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from './entities/user.entity';
+import { GetUser, RawHeaders } from './decorators';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { RoleProtected } from './decorators/role-protected.decorator';
+import { ValidRoles } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -19,10 +24,30 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute() {
+  testingPrivateRoute(
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[],
+  ) {
     return {
       ok: true,
-      message: 'this is a private route'
+      message: 'this is a private route',
+      user,
+      userEmail,
+      rawHeaders
+    };
+  }
+
+
+  @Get('private2')
+  @RoleProtected(ValidRoles.admin, ValidRoles.superUser)
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  testingPrivateRoute2(
+    @GetUser() user: User,
+  ) {
+    return {
+      ok: true,
+      user,
     };
   }
 
---
+++ src/auth/interfaces/valid-roles.ts
@@ -0,0 +1,5 @@
+export enum ValidRoles {
+    admin = 'ADMIN_ROLE',
+    superUser = 'SUPER_USER_ROLE',
+    user = 'USER_ROLE'
+}
